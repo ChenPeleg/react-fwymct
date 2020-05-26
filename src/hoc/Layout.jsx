f23133de14eebbff2afa0519b2ec7139e13fd177
ChenPeleg <chenpeleg@gmail.com>
2020-05-26 19:50:28 +0300

    1 file modified, adder a header component

--- src/hoc/Layout.jsx
+++ src/hoc/Layout.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import "./Layout.scss"
+import Header from "../components/Header/Header"
 class Layout extends Component   {
 render () {
-  return (<div className ="layout"> {this.props.children}</div>)
+  return (
+    <div className ="layout"> <Header/>{this.props.children}</div>)
 }
 }
 export default Layout
